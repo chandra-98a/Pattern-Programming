2d8aaabef6d06df66e7afa53995ef420c37c423c
chandra-98a <chandrakalaa2022@gmail.com>
2023-09-07 15:34:57 +0530

    "Printing a left half diamond using nested for loop "

---
+++ src/com/capgemini/pp/PatternEleven.java
@@ -0,0 +1,20 @@
+package com.capgemini.pp;
+
+public class PatternEleven {
+    public static void main(String[] args){
+        //j=5-i+1
+        for (int i=1;i<=5;i++){
+            for(int j=1;j<=5-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //j=i;
+        for (int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
